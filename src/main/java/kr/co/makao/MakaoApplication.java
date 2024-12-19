d14d7fea055e8c266b2bcd4b3c7c29c0a795863f
Jchan <gkswocks123@gmail.com>
2024-12-19 20:30:03 +0900

    Feat : init

---
+++ src/main/java/kr/co/makao/MakaoApplication.java
@@ -0,0 +1,11 @@
+package kr.co.makao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakaoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MakaoApplication.class, args);
+    }
+}
---
+++ src/test/java/kr/co/makao/MakaoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.makao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
